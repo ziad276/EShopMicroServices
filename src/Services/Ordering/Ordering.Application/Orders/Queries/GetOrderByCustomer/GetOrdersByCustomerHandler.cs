7299385492436c5795daf1e7d27b3a70fd003913
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-17 22:25:55 +0200

    Add GetOrdersByCustomer CQRS query and MediatR handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Ordering.Application.Orders.Queries.GetOrderByCustomer
+{
+    public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext) :
+        IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking() // Use AsNoTracking() when this query is only for **read-only purposes**
+                .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(o => o.OrderName.Value)
+                .ToListAsync(cancellationToken);
+                
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+
+}
