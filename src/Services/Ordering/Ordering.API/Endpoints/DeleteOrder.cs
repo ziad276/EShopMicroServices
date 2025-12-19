207ec4f2c679f3237d9a6ab77e0eb97a3a8b1de1
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-19 23:08:35 +0200

    feat: add GetOrdersByName endpoint to Ordering.API

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,4 +1,7 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints
 {
     //- Accepts the order ID as a parameter.
     //- Constructs a DeleteOrderCommand.
@@ -6,7 +9,27 @@
     //- Returns a success or not found response.
 
     //public record DeleteOrderRequest(Guid Id);
-    public class DeleteOrder
+    public record DeleteOrderResponse(bool IsSuccess);
+
+
+    public class DeleteOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async(Guid Id, ISender sender) =>
+            {
+                var result =  await sender.Send(new DeleteOrderCommand(Id));
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteOrder")
+                .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Order")
+                .WithDescription("Delete Order");
+
+        }
     }
 }
--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,9 +1,29 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Queries.GetOrderByName;
+
+namespace Ordering.API.Endpoints
 {
     //- Accepts a name parameter.
     //- Constructs a GetOrdersByNameQuery.
     //- Retrieves and returns matching orders.
-    public class GetOrdersByName
+    
+    public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByName : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrderByNameQuery(orderName));
+                var response = result.Adapt<GetOrdersByNameResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetOrdersByName")
+                .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Get Orders By Name")
+                .WithDescription("Get Orders By Name");
+        }
     }
 }
