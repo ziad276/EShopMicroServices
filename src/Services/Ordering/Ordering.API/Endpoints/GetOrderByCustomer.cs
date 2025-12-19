f4a3ff2a39160989f21d2e967b3bb5fa2899f263
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-19 23:14:51 +0200

    feat: add GetOrdersByCustomer endpoint to Ordering.API

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrderByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrderByCustomer.cs
@@ -1,10 +1,34 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Queries.GetOrderByCustomer;
+
+namespace Ordering.API.Endpoints
 {
 
     //- Accepts a customer ID.
     //- Uses a GetOrdersByCustomerQuery to fetch orders.
     //- Returns the list of orders for that customer.
-    public class GetOrderByCustomer
+    
+    public record GetOrderByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+
+    public class GetOrderByCustomer : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender send) =>
+            {
+                var result = await send.Send(new GetOrdersByCustomerQuery(customerId));
+
+                var response = result.Adapt<GetOrderByCustomerResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetOrdersByCustomer")
+                .Produces<GetOrderByCustomerResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Get Orders By Customer")
+                .WithDescription("Get Orders By Customer");
+        }
     }
 }
