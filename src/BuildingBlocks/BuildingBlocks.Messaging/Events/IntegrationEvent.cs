d39e798c04ff9b4fd3d26a87faab8d34e88ef513
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-26 20:31:47 +0200

    Add BuildingBlocks.Messaging for BasketCheckoutEvent integration

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.Messaging.Events
+{
+    public record IntegrationEvent
+    {
+        public Guid id => Guid.NewGuid();
+        public DateTime occurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
