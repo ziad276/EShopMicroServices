c008d6af5bbc851d43dfca13d758dec4f2199047
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-26 20:28:59 +0200

    Update shared BuildingBlocks components

--- src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -1,12 +1,6 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BuildingBlocks.Behaviors
 {
     public class LoggingBehavior<TRequest, TResponse>
