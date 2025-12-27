9a3f4c84ab80c42da220113a0bffd23d1206ed8a
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-27 02:03:03 +0200

    Implemented Basket Checkout handler and event publish to RabbitMQ via MassTransit

--- src/Services/Basket.API/Basket/CheckOutBasket/CheckOutBasketEndpoints.cs
+++ src/Services/Basket.API/Basket/CheckOutBasket/CheckOutBasketEndpoints.cs
@@ -1,18 +1,18 @@
 ﻿
 
-namespace Basket.API.Basket.CheckOutBasket
+namespace Basket.API.Basket.CheckoutBasket
 {
 
     public record CheckoutBasketRequest(BasketCheckoutDto BasketCheckoutDto);
     public record CheckoutBasketResponse(bool IsSuccessful);
 
-    public class CheckOutBasketEndpoints : ICarterModule
+    public class CheckoutBasketEndpoints : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
             app.MapPost("basket/checkout", async(CheckoutBasketRequest request, ISender sender) =>
             {
-                var command = request.Adapt<CreateBasketCommand>();
+                var command = request.Adapt<CheckoutBasketCommand>();
                 var result = await sender.Send(command);
                 var response =  result.Adapt<CheckoutBasketResponse>();
 
