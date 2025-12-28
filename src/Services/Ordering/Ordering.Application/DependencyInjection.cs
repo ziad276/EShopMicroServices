d1f520dcf9fdcea3cf24fa37394d2bee1f4439d7
ziad gouhar <ziadgouhar53@gmail.com>
2025-12-28 22:30:42 +0200

    Initial MassTransit and RabbitMQ setup for Ordering application

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -16,6 +17,8 @@ namespace Ordering.Application
                 config.AddOpenBehavior(typeof(ValidationBehavior<,>));
                 config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
+
+            services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly();
 
             return services;
 
