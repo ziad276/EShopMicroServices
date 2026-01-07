28b9f5e0591da2a607292a10e8689b8337102268
ziad gouhar <ziadgouhar53@gmail.com>
2026-01-07 16:16:45 +0200

    Update YARP API gateway, docker configs, solution and add WebApps & local settings

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿
+using Shopping.Web.Models.Catalog;
+
+namespace Shopping.Web.Models.Catalog
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; }= default!;
+        public decimal Price { get; set; } 
+    }
+}
+
+// wrapper classes
+
+public record GetProductRespone(IEnumerable<ProductModel> Products);
+public record GetProductByCategoryRespone(IEnumerable<ProductModel> Products);
+public record GetProductByIdRespone(ProductModel Product);
